b223f1d8e7eb0f53e0f1f7a96cfa19867618f0f9
Faisal Sayed <faisal.sayed502@gmail.com>
2025-03-28 00:47:22 -0400

    tryna fix deployment

--- flask/app.py
+++ flask/app.py
@@ -71,4 +71,4 @@ def predict():
 
 if __name__ == "__main__":
     load_model()  # Load the model before starting the server
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=10000)
